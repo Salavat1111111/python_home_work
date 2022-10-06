d540ffd408a223faaa6c4ab9df0aad953827c545
Salavat Faskhutdinov <salavat_faskhutdinov@MacBook-Air-Salavat.local>
2022-10-07 00:05:40 +0300

    add new file task_4

---
+++ home_work_2/task_4.py
@@ -0,0 +1,16 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+
+with open('file.txt', 'w') as data:
+ data.write("-5\n")
+ data.write("-4\n")
+ data.write("-3\n")
+ data.write("-2\n")
+ data.write("-1\n")
+ data.write("0\n")
+ data.write("1\n")
+ data.write("2\n")
+ data.write("3\n")
+ data.write("4\n")
+ data.write("5\n")
