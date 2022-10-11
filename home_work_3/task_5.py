a98a27427239e392de2264f39d84d47290beb88e
Salavat Faskhutdinov <salavat_faskhutdinov@MacBook-Air-Salavat.local>
2022-10-11 16:48:34 +0300

    add new file

---
+++ home_work_3/task_5.py
@@ -0,0 +1,15 @@
+a=int(input('введите число: ')) 
+b=int(input('введите число: ')) 
+
+f3=1 
+f1=0 
+f2=1 
+for i in range(a): 
+    f1,f2=f2,f1+f2 
+#f1,f3=f3,f1-f3 
+print(f2) 
+#print(f3) 
+f1 = 0
+for j in range (b): 
+    f1,f3=f3,f1-f3 
+print(f3)
