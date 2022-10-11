75668509c75636fe2057ea355dfa2d446080bdf0
Salavat Faskhutdinov <salavat_faskhutdinov@MacBook-Air-Salavat.local>
2022-10-11 15:54:22 +0300

    add new filu

--- home_work_3/task_1.py
+++ home_work_3/task_1.py
@@ -1,16 +1,25 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
-numbers =list(range(1, 11))
-print(numbers)
 
-num = 0
-i = 0
-for i in numbers:
-    if (i % 2 == 0):
-        num = num + 0
-    else:
-        num = num + numbers[i]
-print(num)
+# odd_numbers = []
+# even_numbers = []
+# l = len(numbers)
+
+# for i in range(l):
+#     if ( i % 2 == 0):
+#         even_numbers.append(i)
+#     else:
+#         odd_numbers.append(i)
+# print(f'нечетные числа {odd_numbers}')
+
+
+# for i in range(l):
+#     if i%2==0:
+#         even_numbers.append(numbers[i])
+#     else:
+#         odd_numbers.append(numbers[i])
+# print(even_numbers)
+# 
 
 # while (i <= len(numbers)):
 #     if ( i % 2 == 0):
@@ -23,11 +32,26 @@ print(num)
 
 
 
+# i = 0
+# for i in numbers:
+#     if (i % 2 == 0):
+#         num = num + 0
+#     else:
+#         num = num + numbers[i]
+# print(num)
 
 
+numbers =list(range(1, 11))
+print(numbers)
 
+i = 0
+sum = 0
 
-
+for i in numbers:
+ if (numbers.index(i) % 2 != 0):
+    sum += i
+i+=1
+print(sum)
 
 
 
