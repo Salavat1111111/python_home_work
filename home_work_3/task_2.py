bd69314511b305c321fac0584be38a1d349c1954
Salavat Faskhutdinov <salavat_faskhutdinov@MacBook-Air-Salavat.local>
2022-10-11 16:10:47 +0300

    add new file

--- home_work_3/task_2.py
+++ home_work_3/task_2.py
@@ -5,4 +5,13 @@
 
 
 numbers = list(range(1, 10))
-
+print(numbers)
+i = 0
+j = len(numbers)
+num = 0
+while i < j:
+    num = numbers[i] * numbers[j - 1]
+    print(num)
+    i+=1
+    j-=1
+else: print ("Stop")
